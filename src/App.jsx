64ac99ccaa846b1afb5f16cf24c3122266306ca1
GabrielSathler <gabrielvdfs@gmail.com>
2025-04-28 17:24:30 -0300

    feat: Gemini 2.0 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
+import { useState, useRef, useEffect } from "react";
 import Chatboticon from "./components/Chatboticon"
+import ChatForm from "./components/ChatForm"
+import ChatMessage from "./components/ChatMessage";
 
 const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  const chatBodyRef = useRef();
+
+  const generateBotResponse = async (history) => {
+    //função auxiliar para poder atualizar o historico do chat
+    const updateHistory = (text) => {
+      setChatHistory(prev => [...prev.filter(msg => msg.text !== "Pensando..."), {role: "model", text}]);
+    } 
+
+    //formata o historico do chat para a request da API
+    history = history.map(({role, text})=> ({role, parts: [{text}]}));
+
+    const requestOpitions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({contents: history})
+    }
+
+    try {
+      //faz a chamada da API para que o bot responda
+
+      const response  = await fetch(import.meta.env.VITE_API_URL, requestOpitions);
+      const data = await response.json();
+      if(!response.ok) throw new Error(data.error.message || "Alguma coisa deu erradao!");
+
+      //limpa e atualiza o historico do chat com a resposta do bot
+      const  apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      updateHistory(apiResponseText);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() =>{
+    {/*Scroll automatico para quando o historico atualizar*/}
+    chatBodyRef.current.scrollTo({top: chatBodyRef.current.scrollHeight, behavior: "smooth"});
+  }, [chatHistory]);
+
   return (
     <div className="container">
+      <button id="chatbot-toggler">
+        <span className="material-symbols-outlined">mode_comment</span>
+        <span className="material-symbols-outlined">close</span>
+      </button>
+
       FAZER A LANDING PAGE DEPOIS
       <div className="chatbot-popup">
         {/*HEADER DO CHATBOT*/}
@@ -11,31 +57,27 @@ const App = () => {
             <Chatboticon/>
             <h2 className="logo-text">FuriaAI</h2>
           </div>
-          {/*consertar depois*/} 
           <button className="material-symbols-outlined">
             keyboard_arrow_down 
           </button>
         </div>
-        <div className="chat-body">
+        {/*CHATBOT BODY*/}
+        <div ref={chatBodyRef} className="chat-body">
           <div className="message bot-message">
             <Chatboticon/>
             <p className="message-text">
               Olá fã!👋 <br/> sou o Chatbot da Furia! Me pergunte o que quer saber! Sobre a maior ORG de E-Sports do Brasil! 🐼
             </p>
-          </div>
-          <div className="message user-message">
-            <p className="message-text">
-             Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
+          </div>  
+          {/*RENDERIZA O CHAT DO USUARIO*/}
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat}/>
+          ))}
         </div>
 
-        {/*FOOTER DO CHATBOT*/}
+        {/*FOOTER DO CHATBOT E HISTORICO DE CONVERSA*/}
         <div className="chat-footer">
-          <form action="" className="chat-form">
-            <input type="text" placeholder="mensagem..." className="message-input" required/>
-            <button className="material-symbols-outlined">arrow_upward</button>
-          </form>
+            <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse}/>
         </div>
       </div>
     </div>
--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,6 +1,31 @@
-const ChatForm = () => {
+import { useRef } from "react";
+
+const ChatForm = ({chatHistory, setChatHistory, generateBotResponse}) => {
+    const inputRef = useRef();
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const userMessage = inputRef.current.value.trim();
+        if(!userMessage) return;
+        inputRef.current.value = "";
+        
+        //atualiza o chat com a mensagem do usuario
+        setChatHistory(history => [...history, { role: "user", text: userMessage }]);
+
+        //adiciona um placeholder "pensando" para a resposta do bot
+        setTimeout(() => {
+                setChatHistory(history => [...history, { role: "model", text: "Pensando..." }])
+                
+                // chama a função para gerar a resposta do bot
+                generateBotResponse([...chatHistory, { role: "user", text: userMessage }]);
+            }, 500);
+    }
+
   return (
-    <div>ChatForm</div>
+    <form action="" className="chat-form" onSubmit={handleFormSubmit}>
+        <input ref={inputRef} type="text" placeholder="mensagem..." className="message-input" required/>
+        <button className="material-symbols-outlined">arrow_upward</button>
+    </form>
   )
 }
 
